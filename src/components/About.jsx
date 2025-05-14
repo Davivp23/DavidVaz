0d3c4563619677c8bfa5021d1e5cab47c5c76bec
Davivp23 <davidvazpe23@gmail.com>
2025-05-15 01:39:46 +0200

    Update About component to improve personal introduction text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,9 @@ export default function About() {
   return (
     <section className="my-12">
       <h2 className="text-2xl font-semibold mb-4 text-blue-300">Sobre mí</h2>
-      <p className="text-gray-300">[¡Hola! <br />
+      <p className="text-gray-300">¡Hola! <br />
         Me llamo David y aquí podrás ver varios de los proyectos que he ido haciendo a lo largo de mi vida. <br />
-        Desde pequeño me han gustado las tecnologías en general, pero principalmente la programación.]</p>
+        Desde pequeño me han gustado las tecnologías en general, pero principalmente la programación.</p>
     </section>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,8 @@ export default function Contact() {
   return (
     <section className="my-12">
       <h2 className="text-2xl font-semibold mb-4 text-blue-300">Contacto</h2>
-      <p className="text-gray-300">[Correo: davidvazpe23@gmail.com<br />
-        Instagram: @vaz23david]</p>
+      <p className="text-gray-300">Correo: davidvazpe23@gmail.com<br />
+        Instagram: @vaz23david</p>
     </section>
   );
 }
