0a5d795c2362bd71d0acc3789fb670a45c9dd2e3
Davivp23 <davidvazpe23@gmail.com>
2025-05-15 00:12:38 +0200

    Update About and Contact components with personal information

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,8 @@ export default function Contact() {
   return (
     <section className="my-12">
       <h2 className="text-2xl font-semibold mb-4 text-blue-300">Contacto</h2>
-      <p className="text-gray-300">[Aquí irá tu información de contacto y redes]</p>
+      <p className="text-gray-300">[Correo: davidvazpe23@gmail.com<br />
+        Instagram: @vaz23david]</p>
     </section>
   );
 }
