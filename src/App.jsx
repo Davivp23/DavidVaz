384457674d5323810c8309206b77a2585f6887ed
Davivp23 <davidvazpe23@gmail.com>
2025-05-15 02:20:40 +0200

    TEST 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import ProjectDetail from './components/ProjectDetail';
+// import ProjectDetail from './components/ProjectDetail';
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/proyecto/:id" element={<ProjectDetail />} />
-    </Routes>
-  );
+  return <Home />;
 }
