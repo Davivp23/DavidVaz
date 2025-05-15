2838c6a30ec7c98702907588af613e5404bcf638
Davivp23 <davidvazpe23@gmail.com>
2025-05-15 02:22:52 +0200

    add ProjectDetail import and restore pre-test state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-// import ProjectDetail from './components/ProjectDetail';
+import ProjectDetail from './components/ProjectDetail';
 
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/proyecto/:id" element={<ProjectDetail />} />
+    </Routes>
+  );
 }
