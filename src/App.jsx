8516b9344ed2731e0ee8420b088f8e8c882e5368
Davivp23 <davidvazpe23@gmail.com>
2025-05-15 02:05:10 +0200

    Refactor Home component: move to root directory and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './Home';
 import ProjectDetail from './components/ProjectDetail';
 
 export default function App() {
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 export default function Home() {
   return (
