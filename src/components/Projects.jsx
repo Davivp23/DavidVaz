21066dd0753c893e36889407eca21b9206ddf12a
Davivp23 <davidvazpe23@gmail.com>
2025-05-25 15:08:02 +0200

    Fix technology display in Projects component to join array elements

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,8 @@ export default function Projects({ onHover }) {
             className="block p-4 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] transition duration-300">
             <h3 className="text-xl font-bold text-blue-400">{project.title}</h3>
             <p className="text-gray-300">{project.description}</p>
-            <p className="text-gray-300">{project.technologies}</p>
+            
+            <p className="text-gray-300">{project.technologies.join(', ')}</p>
           </Link>
         ))}
       </div>
