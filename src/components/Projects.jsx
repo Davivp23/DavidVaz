8eb274296ffde698edd5cbaaa8affd3cff9954a4
Davivp23 <davidvazpe23@gmail.com>
2025-05-25 15:37:34 +0200

    Refactor Projects component for improved readability and structure

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,4 @@
+// Projects.jsx
 import React, { forwardRef } from 'react';
 import { Link } from 'react-router-dom';
 import projects from '../data/projects';
@@ -6,17 +7,27 @@ const Projects = forwardRef(({ onHover }, ref) => {
   return (
     <section className="py-10">
       <h2 className="text-2xl font-semibold mb-4 text-blue-300">Proyectos</h2>
-      <div className="flex flex-col gap-6">
+      
+      {}
+      <div ref={ref} className="flex flex-col gap-6">
         {projects.map((project) => (
           <Link
             key={project.id}
             to={`/proyecto/${project.id}`}
             onMouseEnter={() => onHover(project)}
-            className="block p-4 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] transition duration-300">
-            <h3 className="text-xl font-bold text-blue-400" style={{ marginBottom: '1vh' }}>{project.title}</h3>
-            <p className="text-gray-300" style={{ marginBottom: '0.5vh' }}>{project.description}</p>
-            
-            <p className="text-gray-300" style={{ marginBottom: '0.5vh' }}>{project.technologies.join(', ')}</p>
+            className="block p-4 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] transition duration-300"
+          >
+            <h3
+              className="text-xl font-bold text-blue-400"
+              style={{ marginBottom: '1vh' }}
+            >
+              {project.title}
+            </h3>
+            <p className="text-gray-300" style={{ marginBottom: '0.5vh' }}>
+              {project.description}
+            </p>
+            <p className="text-gray-300" style={{ marginBottom: '0.5vh' }}>
+              {project.technologies.join(', ')}</p>
           </Link>
         ))}
       </div>
