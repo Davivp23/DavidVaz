4dc67ed234a810d8e88ebaf1e0137c4887ab7bf4
Davivp23 <davidvazpe23@gmail.com>
2025-07-09 00:10:51 +0200

    more images

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,7 @@ const projects = [
     title: 'Planificador de generadores eléctricos lineales',
     description: 'Una pequeña aplicación web para elegir variables a la hora de crear el generador lineal óptimo.\nEste proyecto puede tardar un poco en cargar, ya que es una aplicación de Streamlit que se ejecuta en un servidor remoto.',
     url: 'https://generadorlineal.onrender.com',
-    image: 'https://pbs.twimg.com/media/Gq5lFEMXwAEpJMJ?format=png&name=small',
+    image: 'https://pbs.twimg.com/media/GvXhVn0WkAAdDw3?format=png&name=900x900',
     technologies: ['Python', 'streamlit', 'matplotlib'],
   },
   {
@@ -20,7 +20,7 @@ const projects = [
     title: 'Este portafolio',
     description: 'Un portafolio personal para mostrar mis proyectos y habilidades.',
     url: 'https://davidvaz.pages.dev',
-    image: 'https://pbs.twimg.com/media/Gq5lFEMXwAEpJMJ?format=png&name=small',
+    image: 'https://pbs.twimg.com/media/GvXid30XAAAYlis?format=jpg&name=large',
     technologies: ['React', 'Vite', 'TailwindCSS', 'FramerMotion', 'JavaScript'],
   },
   // Puedes agregar más aquí
