d19d6c771dbe0bf72498eedb6dc21af592b4483f
Davivp23 <davidvazpe23@gmail.com>
2025-07-08 04:14:09 +0200

    Add spacing for improved layout in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="p-6 text-center text-3xl font-bold text-blue-400">
+      
+      
       David Vaz
+      <p className="text-gray-300 mt-2">
+        Desarrollador FullStack
+      </p>
     </header>
   );
 }
