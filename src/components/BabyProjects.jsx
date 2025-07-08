3f2f53f932ec104b06601b84428613a4a3c0900a
Davivp23 <davidvazpe23@gmail.com>
2025-07-08 21:24:09 +0200

    asdasds

--- src/components/BabyProjects.jsx
+++ src/components/BabyProjects.jsx
@@ -7,7 +7,9 @@ const Projects = forwardRef(({ onHover }, ref) => {
   return (
     <section ref={ref} className="py-10">
       <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400">Proyectos pequeños</h2>
-      <div className="flex flex-col gap-6">
+      <div className="block p-4 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] transition duration-300"
+        key={project.id}
+        onMouseEnter={() => onHover(project)}>
         {projects.map((project) => (
           <CardHeader className="p-0">
             <button
@@ -23,14 +25,6 @@ const Projects = forwardRef(({ onHover }, ref) => {
               className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
             />
             </button>
-              <Link
-                key={project.id}
-                onMouseEnter={() => onHover(project)}
-                className="block p-4 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] transition duration-300">
-                <h3 className="text-xl font-bold text-blue-300" style={{ marginBottom: '1vh' }}>{project.title}</h3>
-                <p className="text-gray-300" style={{ marginBottom: '0.5vh' }}>{project.description}</p>
-                <p className="text-gray-300" style={{ marginBottom: '0.5vh' }}>{project.technologies.join(', ')}</p>
-              </Link>
           </CardHeader>
         ))}
       </div> 
