691bbf31609591d69b650bb54d97880c8f23046d
Davivp23 <davidvazpe23@gmail.com>
2025-07-08 22:52:38 +0200

    I DONT EVEN KNOW

--- src/data/babyProjects.js
+++ src/data/babyProjects.js
@@ -20,6 +20,27 @@ const babyProjects = [
     image: 'https://placehold.co/600x400.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
   },
+  {
+    id: 'TR',
+    title: 'IA Análisis de células cancerígenas',
+    description: 'Trabajo de fin de bachillerato en el que se utiliza una IA para analizar imágenes de células cancerígenas y determinar si son malignas o benignas según su dimensión fractal.',
+    image: 'https://placehold.co/600x400.png',
+    technologies: ['Python', 'Numpy', 'matplotlib', 'tensorflow'],
+  },
+  {
+    id: 'indicador',
+    title: 'Indicador RSI + MACD con señales para TradingView',
+    description: 'Indicador personalizado para TradingView que combina RSI y MACD con señales de compra/venta rentables.',
+    image: 'https://placehold.co/600x400.png',
+    technologies: ['PinescriptV5', 'Python', 'Backtesting', 'ta.lib'],
+  },
+  {
+    id: 'stock',
+    title: 'Aplicación de control de stock y caducidades',
+    description: 'Una aplicación web para gestionar el stock de productos y sus fechas de caducidad con alertas usado en una tienda.',
+    image: 'https://placehold.co/600x400.png',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+  },
   // Puedes agregar más aquí
 ];
 
