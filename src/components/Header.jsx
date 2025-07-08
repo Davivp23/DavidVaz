c67386ea1f060fdb14f7da6356fa7b8ba3c8bdca
Davivp23 <davidvazpe23@gmail.com>
2025-07-08 04:16:01 +0200

    Refactor spacing in Header component for improved layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="p-6 text-center text-3xl font-bold text-blue-400">
-      
-      
+      <br />
+      <br />
       David Vaz
-      <p className="text-gray-300 mt-2">
+      <p className="text-gray-300 mt-1">
         Desarrollador FullStack
       </p>
     </header>
