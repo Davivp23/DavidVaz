cfeea4c18e7d41a99c66a764a2a9fdb1fde8d54f
Davivp23 <davidvazpe23@gmail.com>
2025-07-09 00:41:25 +0200

    added header

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import projects from '../data/projects';
+import Header from './components/Header';
 
 export default function ProjectDetail() {
   const { id } = useParams();
@@ -18,41 +19,44 @@ export default function ProjectDetail() {
   return (
 
     <div>
-      <h1 className="text-3xl font-bold mt-6 text-blue-400">{project.title}</h1>
-      <div className="bg-[#0d1117] text-white min-h-screen px-4 md:px-16 py-10 flex md:flex-row">
-        {/* Columna izquierda */}
-        <div className="md:w-1/2 w-full md:pr-8">
-          <div className="my-6">
-            <img src={project.image} alt={`Captura de ${project.title}`} className="border-2 border-orange-500 rounded-xl overflow-hidden shadow-[0_0_20px_#f9731666]" />
+    <Header />
+      <main className="container mx-auto flex flex-col md:flex-row px-4">
+        <h1 className="text-3xl font-bold mt-6 text-blue-400">{project.title}</h1>
+        <div className="bg-[#0d1117] text-white min-h-screen px-4 md:px-16 py-10 flex md:flex-row">
+          {/* Columna izquierda */}
+          <div className="md:w-1/2 w-full md:pr-8">
+            <div className="my-6">
+              <img src={project.image} alt={`Captura de ${project.title}`} className="border-2 border-orange-500 rounded-xl overflow-hidden shadow-[0_0_20px_#f9731666]" />
+            </div>
+            <div className="flex flex-col gap-2 mb-4">
+              <a 
+                href={project.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition"
+              >
+                Volver a todos los proyectos
+              </a>
+              <a 
+                href="/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition"
+              >
+                Ver sitio en vivo
+              </a>
+            </div>
           </div>
-          <div className="flex flex-col gap-2 mb-4">
-            <a 
-              href={project.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition"
-            >
-              Volver a todos los proyectos
-            </a>
-            <a 
-              href="/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition"
-            >
-              Ver sitio en vivo
-            </a>
-          </div>
-        </div>
 
-        {/* Columna derecha */}
-        <div className="relative md:w-1/2 w-full flex-col items-center justify-center">
-          <p className="text-gray-300 mb-4">{project.description}</p>
-          <p className="text-gray-400 mb-4">
-            <strong>Tecnologías usadas:</strong> {project.technologies.join(', ')}
-          </p>
+          {/* Columna derecha */}
+          <div className="relative md:w-1/2 w-full flex-col items-center justify-center">
+            <p className="text-gray-300 mb-4">{project.description}</p>
+            <p className="text-gray-400 mb-4">
+              <strong>Tecnologías usadas:</strong> {project.technologies.join(', ')}
+            </p>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
