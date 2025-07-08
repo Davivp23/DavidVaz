3585cb194f1fc309689208b0a0360dc4c3d062d0
Davivp23 <davidvazpe23@gmail.com>
2025-07-08 16:47:50 +0200

    Update text color in About and Contact components for improved consistency

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Contact() {
   return (
     <section className="my-12">
-      <h2 className="text-2xl font-semibold mb-4 text-blue-400">Contacto</h2>
-      <p className="text-gray-300">Correo: davivp23@vivaldi.net<br />
+      <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400">Contacto</h2>
+      <p className="pl-2 text-gray-300">Correo: davivp23@vivaldi.net<br />
         Instagram: @vaz23david</p>
     </section>
   );
