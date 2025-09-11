755c0f33afce94bbe10e959935771bd81b50f69b
Davivp23 <57418457+Davivp23@users.noreply.github.com>
2025-09-11 22:46:36 +0200

    Added more dark: classes to CV

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -20,10 +20,10 @@ export default function CV() {
                     {/* Contenedor izquierdo */}
                     <div className="w-full md:flex-[2] flex flex-col justify-between h-full flex-1 gap-4">
                         {/* Formación académica */}
-                        <section className="flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
+                        <section className="dark:bg-[#d2d7ee] flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
                                             hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] 
                                             transition-all duration-300 group overflow-hidden flex flex-col justify-center">
-                        <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Formación académica</h2>
+                            <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Formación académica</h2>
                             <p className="pl-2 text-gray-300">
                                 Ciclo formativo de grado superior - Desarrollo de aplicaciones web (DAW)<br />
                                 Universitat Oberta de Catalunya (UOC) - 2/2025-Presente <br /> <br />
@@ -39,10 +39,10 @@ export default function CV() {
                         </section>
 
                         {/* Experiencia Profesional */}
-                        <section className="flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
+                        <section className="dark:bg-[#d2d7ee] flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
                                             hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] 
                                             transition-all duration-300 group flex flex-col justify-center">
-                        <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Experiencia profesional</h2>
+                            <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Experiencia profesional</h2>
                             <h3 className="pl-2 text-xl font-semibold mb-2 text-blue-400">Desarrollador Freelance - 2025</h3>
                             <p className="pl-2 text-gray-300">
                                 Autohorario<br />
@@ -61,10 +61,10 @@ export default function CV() {
                     {/* Contenedor derecho */}
                     <div className="w-full md:flex-[1] flex flex-col justify-between h-full flex-1 gap-4">
                         {/* Lenguajes */}
-                        <section className="flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
+                        <section className="dark:bg-[#d2d7ee] flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
                                             hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] 
                                             transition-all duration-300 group overflow-hidden flex flex-col justify-center">
-                        <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Lenguajes</h2>
+                            <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Lenguajes</h2>
                             <p className="pl-2 text-gray-300"> 
                                 - Web: HTML, CSS, JavaScript, TypeScript, React, Next.js, Tailwind CSS <br />
                                 - BBDD: SQL, Supabase <br />
@@ -74,10 +74,10 @@ export default function CV() {
                         </section>
 
                         {/* Idiomas */}
-                        <section className="flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
+                        <section className="dark:bg-[#d2d7ee] flex-1 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl 
                                             hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] 
                                             transition-all duration-300 group overflow-hidden flex flex-col justify-center">
-                        <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Idiomas</h2>
+                            <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400 flex flex-col justify-center">Idiomas</h2>
                             <p className="pl-2 text-gray-300"> 
                                 - Castellano: Nativo <br />
                                 - Catalán: Nativo <br />
@@ -88,15 +88,15 @@ export default function CV() {
                     </div>
                 </div>
                 {/* A demás */}
-                <section className="flex flex-col justify-center mt-6 mb-6 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] transition-all duration-300 group overflow-hidden">
-                <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400">A demás</h2>
-                <p className="pl-2 text-gray-300">
-                    A demás de la programación, me gustan muchas otras cosas: <br />
-                    - Los coches: actualmente estoy restaurando un Volkswagen Golf clásico y me encanta conducir <br />
-                    - La ingeniería: disfruto creando soluciones de todo tipo, pero especialmente la mecánica y electrónica (a parte de la programación) <br />
-                    - El trading: tanto el manual como el algorítmico, y en todos los mercados <br />
-                    - La música: empecé a tocar el piano con 5 años en el liceo y desde entonces no he podido parar <br />
-                </p>
+                <section className="dark:bg-[#d2d7ee] flex flex-col justify-center mt-6 mb-6 p-8 bg-[#0a0f1c] border-2 border-blue-500 rounded-2xl hover:border-orange-500 hover:shadow-[0_0_20px_#ff6b35aa] transition-all duration-300 group overflow-hidden">
+                    <h2 className="pl-2 text-2xl font-semibold mb-4 text-blue-400">A demás</h2>
+                    <p className="pl-2 text-gray-300">
+                        A demás de la programación, me gustan muchas otras cosas: <br />
+                        - Los coches: actualmente estoy restaurando un Volkswagen Golf clásico y me encanta conducir <br />
+                        - La ingeniería: disfruto creando soluciones de todo tipo, pero especialmente la mecánica y electrónica (a parte de la programación) <br />
+                        - El trading: tanto el manual como el algorítmico, y en todos los mercados <br />
+                        - La música: empecé a tocar el piano con 5 años en el liceo y desde entonces no he podido parar <br />
+                    </p>
                 </section>
             </main>
         </div>
